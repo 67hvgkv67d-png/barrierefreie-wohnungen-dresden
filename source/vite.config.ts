3ec37d0b64b6c6fd62513623b388364819981d30
67hvgkv67d-png <67hvgkv67d@privaterelay.appleid.com>
2026-09-02 15:32:20 +0200

    Add mini maps to RB listings

--- source/vite.config.ts
+++ source/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const assetVersion = "20260902-1";
+const assetVersion = "20260902-2";
 
 export default defineConfig({
   plugins: [
